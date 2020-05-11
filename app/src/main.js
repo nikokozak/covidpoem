2df426013a9b7906f685c077d6a338f5eda00537
Nikolai Kozak <nikokozak@gmail.com>
2020-05-11 15:44:10 -0400

    Added libraries (Papa), requests.

--- app/src/main.js
+++ app/src/main.js
@@ -1,5 +1,23 @@
 const axios = require('axios').default
+import Papa from './papaparse.min.js'
 
+axios.get('https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto5/TotalesNacionales_T.csv')
+.then( response => {
+	
+	let handleError = (err, f) => { console.log(f) }
 
+	let result = Papa.parse(response.data, 
+		{ skipEmtpyLines: true, 
+			header: true, 
+			dynamicTyping: true,
+			error: handleError,
+		});
+
+	let error = result.errors[0].row
+
+	result.data.splice(error, 1)
+	
+	console.log(result);
+});
 
 console.log("Hey!");
