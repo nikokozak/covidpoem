7ebd37ff06a93dcc787ecf9cc94c33c1015ccd57
Nikolai Kozak <nikokozak@gmail.com>
2020-05-11 21:25:52 -0400

    Added parsing utils, refac

--- app/src/main.js
+++ app/src/main.js
@@ -1,6 +1,6 @@
 const axios = require('axios').default
 import Papa from './papaparse.min.js'
-import helpers from './utils.js'
+import _h from './utils.js'
 
 axios.get('https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto5/TotalesNacionales_T.csv')
 .then( response => {
@@ -11,9 +11,26 @@ axios.get('https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/out
 			dynamicTyping: true,
 		});
 
-	helpers.handleErrors(result)
+	_h.handleParseErrors(result)
 
 	console.log(result);
+
+	let latest = _h.getLatestData(result)
+
+	_h.changeCount("totalCount", _h.getTotalCount(latest))
+	_h.changeCount("newCount", _h.getNewCount(latest))
+	_h.changeCount("activeCount", _h.getActiveCount(latest))
+	_h.changeCount("recoveredCount", _h.getRecoveredCount(latest))
+	_h.changeCount("deadCount", _h.getDeadCount(latest))
+	
+	let deadYesterday = _h.diffDataFromEnd(0, 1, 'Fallecidos', result)
+	let newCountDiff = _h.diffDataFromEnd(0, 1, 'Casos nuevos totales', result)
+
+	_h.changeCount("newCountYesterday", Math.abs(newCountDiff))
+	_h.changeText('switchNew', _h.switchFormat(newCountDiff, 'mas', 'menos'))
+
+	_h.changeCount("yesterdayDeadCount", deadYesterday)
+
 });
 
 console.log("Hey!");
--- app/src/utils.js
+++ app/src/utils.js
@@ -1,10 +1,10 @@
 // UTILITIES AND HELPERS
 
-const helpers = (function() {
+const _h = (function() {
 
 	// Handle errors thrown by Papa while parsing.
 	// Use instead of Papa callback.
-	function handleErrors (parseObj) {
+	function handleParseErrors (parseObj) {
 		
 		let errorRows = parseObj.errors.map(x => x.row)
 		errorRows.sort((a, b) => b - a)
@@ -17,12 +17,143 @@ const helpers = (function() {
 
 	}
 
+	// Format a number by adding decimal places (returns a string)
+	function formatNumber (number) {
+		
+		return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+	}
+
+	// Change the text of a given DOM element
+	function changeText (id, newText) {
+
+		let element = document.getElementById(id)
+
+		element.innerHTML = newText
+
+		return element
+
+	}
+	
+	// Get the latest data entry from the Papa object data array.
+	function getLatestData (parseObj, format = true) {
+
+		let result = parseObj.data[parseObj.data.length - 1]
+	
+		return format ? formatNumber(result) : result
+
+	}
+
+	// Return a slice from the tail of the Papa object data array.
+	function getTailData (parseObj, count = 1, includeLast = false) {
+	
+		let data = parseObj.data
+
+		let sliceStart = data.length - 1 - count
+		let sliceEnd = data.length
+
+		return includeLast ? data.slice(sliceStart, sliceEnd) : data.slice(sliceStart, sliceEnd - 1)
+
+	}
+	
+	// Simple ternary - if positive return first arg, if not second.
+	function switchFormat (value, optionPos, optionNeg) {
+		
+		return value >= 0 ? optionPos : optionNeg
+
+	}
+
+	// Calculate the difference between two data rows using an index relative to the end
+	// of the Papa data array.
+	function diffDataFromEnd (row1, row2, column, parseObj, format = false) {
+
+		let data = parseObj.data
+		let index = parseObj.data.length - 1
+
+		let rowa = data[index - row1]
+		let rowb = data[index - row2]
+
+		result = rowa[column] - rowb[column]
+
+		return format ? formatNumber(result) : result		
+	}
+
+	// Get the total count from a single Papa data object.
+	function getTotalCount (parseData, format = true) {
+
+		let result = parseData['Casos totales']
+
+		return format ? formatNumber(result) : result	
+	}
+
+	function getActiveCount (parseData, format = true) {
+
+		let result = parseData['Casos activos']
+
+		return format ? formatNumber(result) : result	
+	}
+
+	// Get the new cases count from a single Papa data object.
+	function getNewCount (parseData, format = true) {
+	
+		let result = parseData['Casos nuevos totales']
+
+		return format ? formatNumber(result) : result
+	}
+
+	// Get the recovered cases from a single Papa data object.
+	function getRecoveredCount (parseData, format = true) {
+
+		let result = parseData['Casos recuperados']
+
+		return format ? formatNumber(result) : result
+	}
+
+	// Get the death count from a single Papa data object.
+	function getDeadCount (parseData, format = true) {
+
+		let result = parseData['Fallecidos']
+
+		return format ? formatNumber(result) : result
+	}
+
+	// Get the date string from a single Papa data object.
+	function getDate (parseData) {
+
+		return parseData['Fecha']
+
+	}
+	
+	// Change the count displayed in a given id tag.
+	function changeCount (id, newCount) {
+		
+		let element = document.getElementById(id)
+
+		element.innerHTML = newCount
+
+		return element
+
+	}
+
 	return {
 		// Make public
-		handleErrors,
+		handleParseErrors,
+		formatNumber,
+		changeText,
+		changeCount,
+		switchFormat,
+		diffDataFromEnd,
+		getTailData,
+		getLatestData,
+		getTotalCount,
+		getActiveCount,
+		getNewCount,
+		getRecoveredCount,
+		getDeadCount,
+		getDate,
 
 	}
 
 })()
 
-export default helpers 
+export default _h 
